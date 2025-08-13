a987f912cbd3cec33343f3eaa35f0f39d5dadc9e
Lina <Bouyahyaoui.Lina.7@gmail.com>
2025-08-13 19:44:48 +0100

    part 1 Unicafe done

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -1,34 +1,96 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Display =({text})=> {
+  return (
+    <h1>{text}</h1>
+  )
+
+}
+const Button =(props) =>{
+  return(
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+const Line =(props) =>{
+  return(
+    <p>{props.text} {props.value}</p>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const sum = good + neutral + bad;
+  const average = sum ? (good - bad) / sum : 0;
+  const positive = sum ? (good / sum) * 100 : 0;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{sum}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addgood = ()=> {
+    setGood(good+1)
+  }
+  const addneutral = ()=> {
+    setNeutral(neutral+1)
+  }
+  const addbad = ()=> {
+    setBad(bad+1)
+  }
+
+  return (
+    <div>
+      <Display text={"give feedback"}/>
+      <Button onClick={addgood} text={"good"}/>
+      <Button onClick={addneutral} text={"neutral"}/>
+      <Button onClick={addbad} text={"bad"}/>
+
+
+
+      {(good || bad || neutral) ? (<Statistics good={good} neutral={neutral} bad={bad}/>) : (<p>No feedback given</p>)}
+
+
+
+    </div>
   )
 }
 
